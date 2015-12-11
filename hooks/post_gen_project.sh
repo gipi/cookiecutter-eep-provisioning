cc9ce5861526291fb9fa43594bef3eab431de2ff
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2015-12-11 21:41:37 +0100

    Add more variables.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 find ./ -iname 'delete-*' | xargs rm -vfr
+SECRET="$(cat /dev/urandom | LC_ALL=C tr -cd [a-zA-Z0-9] | head -c 12 )"
+
+cat >> ansible_deploy_variables <<EOF
+db_password: '${SECRET}'
+EOF
