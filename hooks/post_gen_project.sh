5fb3ec8f995b9980e00bc0f1bb7d0c73c33e64df
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2015-12-13 15:12:40 +0100

    Generate on fly ssh key if webapp_public_key is empty.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,6 +3,16 @@
 find ./ -iname 'delete-*' | xargs rm -vfr
 SECRET="$(cat /dev/urandom | LC_ALL=C tr -cd [a-zA-Z0-9] | head -c 12 )"
 
+# generate on fly the password for the db
 cat >> ansible_deploy_variables <<EOF
 db_password: '${SECRET}'
 EOF
+
+# generate ssh key for the deploy with empty passphrase
+{% if cookiecutter.webapp_public_key == "" %}
+KEYNAME="id_rsa_{{ cookiecutter.repo_name }}"
+ssh-keygen -f "${KEYNAME}" -N ""
+cat >> ansible_deploy_variables <<EOF
+webapp_public_key: '${KEYNAME}.pub'
+EOF
+{% endif %}
