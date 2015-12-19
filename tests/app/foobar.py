44ece5698570297c79a90d4dd41852585287a9c0
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2015-12-19 22:27:53 +0100

    Improve WSGI application.
    Now the celery task is "long" running.

--- tests/app/foobar.py
+++ tests/app/foobar.py
@@ -1,3 +1,27 @@
+# Very simple WSGI application
+#
+# Partly inspired from <http://lucumr.pocoo.org/2007/5/21/getting-started-with-wsgi/>
+from app.tasks import very_long_task
+from cgi import parse_qs, escape
+import random
+import string
+
+
+# https://stackoverflow.com/questions/14355409/getting-the-upload-file-content-to-wsgi
 def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b"Hello World"]
+    # first of all, launch the long running task in background
+    filename = ''.join([random.choice(string.lowercase) for _ in range(40)])
+    very_long_task.delay(filename)
+
+    # then generate the page with a simple message
+    parameters = parse_qs(env.get('QUERY_STRING', ''))
+    response_text = b'Hello %s: we are generating file %s. We need only 1 minute.' % ('world' if not 'user' in parameters else escape(parameters['user'][0]), filename)
+    start_response(
+        '200 OK',
+        [('Content-Type','text/html')])
+    return [response_text]
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
--- tests/app/tasks.py
+++ tests/app/tasks.py
@@ -1,8 +1,16 @@
 # http://docs.celeryproject.org/en/latest/getting-started/first-steps-with-celery.html#application
 from celery import Celery
+import time
+import os
+
 
 app = Celery('tasks', broker='redis://localhost')
 
 @app.task
-def add(x, y):
-    return x + y
+def very_long_task(filename):
+    time.sleep(60)
+    filepath = os.path.join('/tmp', filename)
+    with open(filepath, 'w+') as f:
+        f.write('foobar')
+
+    return 'file created %s' % filepath
